5ff83f027874ad6d67b6ec62b03b4dc4fe628f81
Jasim Lika <slika25@proton.me>
2024-11-30 00:15:28 +0100

    initial commit

--- backend/config.py
+++ backend/config.py
@@ -7,6 +7,6 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', secrets.token_hex(32))
-    DEVCYCLE_SDK_KEY = os.environ.get('DEVCYCLE_SDK_KEY', 'dvc_server_9107b48a_4c83_4012_a121_22a661e8a304_bff019f')
+    DEVCYCLE_SDK_KEY = os.environ.get('DEVCYCLE_SDK_KEY', secrets.token_hex(32))
     DEBUG = True
 
